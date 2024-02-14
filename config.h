08c0864053e872ca1b1b6c3c99b655eccc2055cf
Aaron John <unrealapex@proton.me>
2024-02-14 10:24:23 -0800

    format: fix comment alignment

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*                fg         bg       */
 	[SchemeNorm] = { "#d9d0d0", "#0d0d0d" },
 	[SchemeSel] = { "#d9d0d0", "#bf2a45" },
 	[SchemeOut] = { "#000000", "#00ffff" },
